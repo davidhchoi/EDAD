835ceb080e552d2b1a89658fa0bb8b4356c236c7
Mamick <mamick20022002@yahoo.com>
2014-01-19 19:20:14 -0500

    Added job descriptions

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,67 @@
-
 <article>
-    <p>Hi and welcome to my website! As you might have guessed by the header, the website name, and the URL, my name is David Choi and I am a Computer Science student at the University of Waterloo. I hope you enjoy the site!</p>
-</article>
-
-<article>
-    <h2>Indochino</h2>
-    <a href="http://www.indochino.com/" class="article-img"><img src="<?= IMG . 'indochino.png' ?>" alt="indochino"></a>
+    <h2>Indochino Apparel Inc.</h2>
+    <article>
+        <a href="http://www.indochino.com/" class="article-img"><img src="<?= IMG . 'indochino.png' ?>" alt="indochino"></a>
+        <b>Position:</b>
+        <p>Software Developer Intern</p>
+        <b>Duration:</b>
+        <p>April 2013 - August 2013</p>
+        <b>Website:</b>
+        <p><a href="http://www.indochino.com/">www.indochino.com</a></p>
+    </article>
+    <br class="clear" />
+    <h4>Description</h4>
+    <p>Back-end engineer focusing on supporting both the manufacturing team and the core 
+        customer experience logic. I created and modified systems to manage inventory, 
+        quality control, and shipping data on tens of thousands of orders. I also dealt 
+        with customer interfaces, the underlying framework of the entire site, database 
+        management, and supporting front-end engineers. </p>
+    <p>Some major projects I played a large role in include a designing and implementing 
+        a statistical algorithmic system to improve customer data (working with several 
+        manufacturing centres), significant framework update and code refactoring project 
+        that removed tens of thousands of lines of code, and designing and implementing 
+        a system to receive and process large amounts of customer feedback in order to 
+        receive meaningful information.</p>
 </article>
 
 <article>
     <h2>Programming Competitions</h2>
-    <img src="<?= IMG . 'code.png' ?>" alt="code" class="article-img">
+    <article>
+        <img src="<?= IMG . 'code.png' ?>" alt="code" class="article-img">        
+        <b>Notable Achievements:</b>
+        <ul>
+            <li>First Place in Canada in the Canadian Computing Competition (CCC) Senior Division 2012</li>
+            <li>Silver Medal in the Final Stage of the CCC 2011, 2012</li>
+            <li>Top 10 in Waterloo 2013 ACM Contest</li>
+        </ul>
+    </article>
+    <h4>Description</h4>
+    <p>Since entering High school, I have competed in many different programming 
+        competitions. Because of them, I developed my problem solving skills, ability 
+        to work under pressure, and experience with implanting theoretical ideas. I 
+        also gained significant familiarity with numerous algorithms and data structures 
+        through studying for these contests in fields like machine learning, compression, 
+        and graph theory, as well as the ability to design efficient algorithms for 
+        problems that I have never seen before.</p>
 </article> 
 
 <article>
-    <h2>Teaching Assistant</h2>
-</article>
-
-<article>
     <h2>Vancouver Model United Nations</h2>
-    <a href="http://vmun.com/" class="article-img"><img src="<?= IMG . 'vmun.png' ?>" alt="vmun"></a>
+    <article>
+        <a href="http://vmun.com/" class="article-img"><img src="<?= IMG . 'vmun.png' ?>" alt="vmun"></a>
+        <b>Position:</b>
+        <p>Chief of Technology</p>
+        <b>Duration:</b>
+        <p>June 2011 – February 2012</p>
+        <b>Website:</b>
+        <p><a href="http://vmun.com/">vmun.com</a></p>
+    </article>
+    <br class="clear" />
+    <h4>Description</h4>
+    <p>Model United Nations is a political simulation conference in which students act 
+        as delegates to the UN and debate and attempt to solve world issues. I designed, 
+        updated, and managed the Vancouver Model United Nations (VMUN) internet presence 
+        that is still being used today by thousands of people today. To do this, I used 
+        various front and back end technologies like HTML, CSS, WordPress, PHP, and MySQL 
+        to create the website, email accounts, registration system, and server.</p>
 </article>
